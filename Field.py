f1de1b3f1b8c2399b099b390f37b3ab0fcd254be
student yandex <yandex@sivilla.com>
2025-01-22 18:53:55 +0300

    add fixing figure, choice new figure from array

--- Field.py
+++ Field.py
@@ -1,18 +1,19 @@
-from random import randint
+from random import choice
 from pygame.locals import *
 
-fig_w, fig_h = 5, 5 #Шаблон размера фигуры
+fig_w, fig_h = 5, 5  # Шаблон размера фигуры
+
 
 class Field:  # Класс поля
-    shapes = {
-        'I': [0, 1],
-        'J': [0, 1, 2, 3],
-        'L': [0, 1, 2, 3],
-        'O': [0],
-        'S': [0, 1],
-        'T': [0, 1, 2, 3],
-        'Z': [0, 1]
-    }  # Названия всех вариантов фигур со всеми вариантами расположения,
+    shapes = list({
+                      'I': [0, 1],
+                      'J': [0, 1, 2, 3],
+                      'L': [0, 1, 2, 3],
+                      'O': [0],
+                      'S': [0, 1],
+                      'T': [0, 1, 2, 3],
+                      'Z': [0, 1]
+                  }.items())  # Названия всех вариантов фигур со всеми вариантами расположения,
 
     # считать нулевым вертикальное положение "головой" вверх
 
@@ -27,9 +28,15 @@ class Field:  # Класс поля
         for y in range(self.height):
             for x in range(self.width):
                 match self.board[y][x]:
-                    case 2, 3:
+                    case 2:
                         if self.board[y + 1][x] == 1:
                             self.new_figure()
 
     def new_figure(self):  # "Затвердевание старой и появление новой фигуры"
-        pass
+        for y in range(self.height):  # Фиксирование всех клеток
+            for x in range(self.width):
+                if self.board[y][x] in [2, 3]:
+                    self.board[y][x] = 1
+
+        figure_next = choice(self.shapes)  # Выбор новой фигуры
+        cur_turn = choice(figure_next[1])
--- main.py
+++ main.py
@@ -6,34 +6,38 @@ from utils import terminate
 import pygame as pg
 import time, random, sys
 from pygame.locals import *
+from Field import Field
 
 WIDTH, HEIGHT = SIZE = 960, 720
-FPS = 25 # Кадры в секунду
-block, zone_h, zone_w = 20, 20, 10 # к block привязываются остальные параметры игрового поля(zone_h, zone_w)
-side_freq, down_freq = 0.15, 0.1 # cкорость с которой поворачиваются и падает фигура
-side_margin = int((WIDTH - zone_w * block) / 2) #Константа. Определяет дистанцию между правой и левой сторонами окна
-top_margin = WIDTH - (zone_h * block) - 5 #Другая константа. Определяет дистанцию между верхом и низом сторонами окна
+FPS = 25  # Кадры в секунду
+block, zone_h, zone_w = 20, 20, 10  # к block привязываются остальные параметры игрового поля(zone_h, zone_w)
+side_freq, down_freq = 0.15, 0.1  # cкорость с которой поворачиваются и падает фигура
+side_margin = int((WIDTH - zone_w * block) / 2)  # Константа. Определяет дистанцию между правой и левой сторонами окна
+top_margin = WIDTH - (zone_h * block) - 5  # Другая константа. Определяет дистанцию между верхом и низом сторонами окна
+
 
 def start_game():
+    points = 0  # Очки
+    f = Field()
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 terminate()
         pg.display.flip()
-        last_move_down = time.time() #Последнее движение вниз
-        last_side_move = time.time() #Последнее движение в сторону
-        last_one = time.time() # Последняя падавшяя фигура
-        falling_down = False # Падает ли сейчас
-        falling_left = False # Двигается ли влево
-        falling_right = False # Двигается ли вправ
-        points = 0 # Очки
+        last_move_down = time.time()  # Последнее движение вниз
+        last_side_move = time.time()  # Последнее движение в сторону
+        last_one = time.time()  # Последняя падавшяя фигура
+        falling_down = False  # Падает ли сейчас
+        falling_left = False  # Двигается ли влево
+        falling_right = False  # Двигается ли вправ
+        f.new_figure()
+        pg.time.Clock().tick(FPS)
 
 
 def main():
     pg.init()
     screen = pg.display.set_mode(SIZE)
     start_game()
-
 
 
 if __name__ == '__main__':
