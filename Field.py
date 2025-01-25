2a0a243bcb376ce69b3c72ab4bf0929af8fbb459
student yandex <yandex@sivilla.com>
2025-01-25 18:28:25 +0300

    add falling block

--- Field.py
+++ Field.py
@@ -1,5 +1,6 @@
 from random import choice
 from pygame.locals import *
+from copy import deepcopy
 
 fig_w, fig_h = 5, 5  # Шаблон размера фигуры
 
@@ -21,22 +22,35 @@ class Field:  # Класс поля
         self.size = self.width, self.height = size
         if self.width < 4 or self.height < 5:
             self.size = self.width, self.height = (10, 15)
-        self.board = [[0] * self.width] * self.height
+        self.board = [[0] * self.width for _ in range(self.height)]
+        self.new_board = [[0] * self.width for _ in range(self.height)]
         self.new_figure()
 
     def update(self):  # Следующий кадр
-        for y in range(self.height):
-            for x in range(self.width):
-                match self.board[y][x]:
-                    case 2:
-                        if self.board[y + 1][x] == 1:
-                            self.new_figure()
+        self.new_board = [[0] * self.width for _ in range(self.height)]
+        try:
+            for y in range(self.height):
+                for x in range(self.width):
+                    match self.board[y][x]:
+                        case 2:
+                            if self.board[y + 1][x] == 1:
+                                raise OverlayError
+                            else:
+                                self.new_board[y + 1][x] = 2  # Сдвиг блока вниз
+        except OverlayError:
+            self.new_figure()
+        self.board = deepcopy(self.new_board)
 
     def new_figure(self):  # "Затвердевание старой и появление новой фигуры"
+        self.new_board = deepcopy(self.board)
         for y in range(self.height):  # Фиксирование всех клеток
             for x in range(self.width):
                 if self.board[y][x] in [2, 3]:
-                    self.board[y][x] = 1
+                    self.new_board[y][x] = 1
 
-        figure_next = choice(self.shapes)  # Выбор новой фигуры
+        figure_next = choice(self.shapes)  # Выбор новой фигуры, вид:(Буквенное обозначение, список всех её положений)
         cur_turn = choice(figure_next[1])
+
+
+class OverlayError(Exception):
+    pass
