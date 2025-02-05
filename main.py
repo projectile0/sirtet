ba6e0328a7e9d5255b917ceb914c82681f403731
Valten <victor.sergeeff@gmail.com>
2025-02-05 18:29:29 +0300

    added database and changed score_screen() start process

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pprint import pp
 import sqlite3
 
 import pygame as pg
+import pygame_menu
 
 from Field import Field
 from utils import terminate
@@ -11,53 +12,54 @@ WIDTH, HEIGHT = SIZE = 960, 720
 FPS = 25  # Кадры в секунду
 FALL_INTERVAL = 0.7  # Интервал между обновлением поля в секундах
 
+
 def start_game(screen):
     points = 0  # Очки
     f = Field()
     FALLEVENT = pg.USEREVENT + 1
     pg.time.set_timer(FALLEVENT, int(FALL_INTERVAL * 1000))
     screen.fill((255, 255, 255))
-    score_displayed = False #Расположен ли экран с очками или нет
+    score_displayed = False  # Расположен ли экран с очками или нет
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 terminate()
             if event.type == FALLEVENT:
                 f.update()
-
                 pp(f.board)  # Отображение таблицы в консоли(ТЕСТ)
-
-            if event.type == pg.QUIT:
-                terminate()
-            if event.type == pg.MOUSEBUTTONDOWN: # Экран выводится по нажатию кнопки (Сделал для того, чтоб хотяб какое то событие было)
-                if event.button == 1:  # Левая кнопка мыши
-                    score_displayed = True  # Устанавливаем флаг, чтобы показать экран счета
-
-            if score_displayed: # Проверка флага
-                score_screen(screen) # Вывод
 
         pg.display.flip()
         pg.time.Clock().tick(FPS)
 
-def score_screen(screen): # Окно с выводом ников и очков игроков
-    conn = sqlite3.connect('Sirtet_score') # Подключение бд
-    cursor = conn.cursor() # Создание курсора
-    cursor.execute("SELECT nick, score FROM data") # Запрос
-    scores = cursor.fetchall() # возвращение записей
-    font = pg.font.Font(None, 36) # шрифт
-    for index, (nick, score) in enumerate(scores): # цикл для вывода
-        text = f"{nick}: {score}" #Форма текста и сам текст
-        text_surface = font.render(text, True, (0, 0, 0)) #Вид текста
-        text_rect = text_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 + index * 40)) # Позиция текста(центр)
-        screen.blit(text_surface, text_rect) # Вывод текста на экран
-    conn.close() # Закрытие связи
+
+def score_screen(screen):  # Окно с выводом ников и очков игроков
+    conn = sqlite3.connect('sirtet_score.db')  # Подключение бд
+    cursor = conn.cursor()  # Создание курсора
+    cursor.execute("SELECT name, score FROM data ORDER BY score DESC")  # Запрос
+    scores = cursor.fetchall()  # возвращение записей
+    font = pg.font.Font(None, 36)  # шрифт
+    for index, (nick, score) in enumerate(scores):  # цикл для вывода
+        text = f"{nick}: {score}"  # Форма текста и сам текст
+        text_surface = font.render(text, True, (0, 0, 0))  # Вид текста
+        text_rect = text_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 + index * 40))  # Позиция текста(центр)
+        screen.blit(text_surface, text_rect)  # Вывод текста на экран
+    conn.close()  # Закрытие связи
     pg.display.flip()
-    pg.time.Clock().tick(FPS)
+    waiting = True
+    while waiting: # Цикл в ожидании нажатия
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                terminate()
+            if event.type == pg.KEYDOWN:
+                waiting = False
+
 
 def main():
     pg.init()
     screen = pg.display.set_mode(SIZE)
     start_game(screen)
+    score_screen(screen)
+
 
 if __name__ == '__main__':
     main()
